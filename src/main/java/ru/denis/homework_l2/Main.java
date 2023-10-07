ffecad4a8f56c6652c3b60f0609933881c15dc80
mctapok <vektor.ct@gmail.com>
2023-10-07 11:33:20 +0300

    feat: update methods name

--- src/main/java/ru/denis/homework_l2/Main.java
+++ src/main/java/ru/denis/homework_l2/Main.java
@@ -2,31 +2,31 @@ package ru.denis.homework_l2;
 
 public class Main {
 
-    private static long[] fiboCashed;
+    private static long[] fibonacciCashed;
 
     public static void main(String[] args) {
 
         int num = 92;
 
-        fiboCashed = new long[num + 1];
+        fibonacciCashed = new long[num + 1];
 
         for (int i = 0; i < num; i++) {
-            System.out.print(fibo(i) + " | ");
+            System.out.print(fibonacci(i) + " | ");
         }
     }
 
-    private static long fibo(int num) {
+    private static long fibonacci(int num) {
         if (num <= 1) {
             return num;
         }
 
-        if (fiboCashed[num] != 0) {
-            return fiboCashed[num];
+        if (fibonacciCashed[num] != 0) {
+            return fibonacciCashed[num];
         }
 
-        long nthFiboNum = (fibo(num - 1) + fibo(num - 2));
+        long nthFiboNum = (fibonacci(num - 1) + fibonacci(num - 2));
 
-        fiboCashed[num] = nthFiboNum;
+        fibonacciCashed[num] = nthFiboNum;
 
         return nthFiboNum;
     }
